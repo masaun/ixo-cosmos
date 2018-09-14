93ae607e5bdfbd0f95b7c8752228eea92fe70895
source-code-smith <herman@sourcecodesmith.com>
2018-09-14 04:23:24 +0200

    HEX encoding

--- src/x/project/cmd/tx.go
+++ src/x/project/cmd/tx.go
@@ -124,29 +124,30 @@ func UpdateProjectStatusCmd(cdc *wire.Codec) *cobra.Command {
 		Short: "Update a a project status and ethereum funding txn id signed by the sovrinDID of the project",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.NewCoreContextFromViper()
-			if len(args) != 3 || len(args[0]) == 0 || len(args[1]) == 0 || len(args[2]) == 0 {
-				return errors.New("You must provide the agentDid, role and the projects private key")
+			if len(args) != 4 || len(args[0]) == 0 || len(args[1]) == 0 || len(args[2]) == 0 || len(args[3]) == 0 {
+				return errors.New("You must provide the parameters")
 			}
 
 			projectStatus := project.ProjectStatus(args[0])
-			ethFundingTxnID := args[1]
-			projectDid := unmarshalSovrinDID(args[2])
-
 			if projectStatus != project.NullStatus && projectStatus != project.PendingStatus && projectStatus != project.FundedStatus && projectStatus != project.StartedStatus && projectStatus != project.StoppedStatus && projectStatus != project.PaidoutStatus {
 				return errors.New("The status must be one of 'CREATED', 'PENDING', 'FUNDED', 'STARTED', 'STOPPED', 'PAIDOUT'")
 			}
+
+			ethFundingTxnID := args[1]
+
+			// projectID := args[2]
 
 			updateProjectDoc := project.UpdateProjectStatusDoc{
 				Status:          projectStatus,
 				EthFundingTxnID: ethFundingTxnID,
 			}
 
-			sovrinDid := unmarshalSovrinDID(args[4])
+			projectDid := unmarshalSovrinDID(args[3])
 
 			// create the message
 			msg := project.NewUpdateProjectStatusMsg(updateProjectDoc, projectDid)
 
-			return ixoSignAndBroadcast(cdc, ctx, msg, sovrinDid)
+			return ixoSignAndBroadcast(cdc, ctx, msg, projectDid)
 		},
 	}
 }
